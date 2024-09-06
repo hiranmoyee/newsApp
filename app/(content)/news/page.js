74f31e89c7ecfbb53a58a34a8055c1e3e1044063
hiranmoyee <hiranmoyeedehingia@gmail.com>
2024-09-07 00:40:24 +0530

    Initial commit fo news app demo

---
+++ app/(content)/news/page.js
@@ -0,0 +1,22 @@
+//best way to fetch data(standard way)
+
+import NewsList from "@/components/news-list";
+import { getAllNews } from "@/lib/news";
+
+export default async function NewsPage() {
+    // const response =  await fetch('http://localhost:8080/news');
+    
+    // if(!response.ok) {
+    //     throw new Error('Failed to fetch news.');
+    // }
+
+    // const news = await response.json();
+
+    const news = await getAllNews();
+    return (
+        <>
+        <h1>News page</h1>
+        <NewsList news={news}/>
+        </>
+    )
+}
---
+++ app/api/route.js
@@ -0,0 +1,11 @@
+//api handler
+//the fundtion name in route handler must have only http method name which is(GET, POST, PATCH, DELETE, PUT)
+
+//export function POST(request){}
+
+// export function GET(request) {
+//     console.log(request);
+
+//     //return Response.json();
+//     return new Response('Hello!');
+// }
